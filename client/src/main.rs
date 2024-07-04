a1439552ed72c708acc8ac748d90a94729e940e1
Joel Liu <83172559+csdtowards@users.noreply.github.com>
2024-07-04 16:24:11 +0800

    Integrate with encoder (#3)

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+use clap::{arg, command};
 use grpc::retriever::{retriever_client::RetrieverClient, BlobRequest};
 
 #[tokio::main]
@@ -7,20 +8,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // enable backtraces
     std::env::set_var("RUST_BACKTRACE", "1");
 
-    let data_root =
-        &hex::decode("1111111111111111111111111111111111111111111111111111111111111111").unwrap();
-    let epoch = 38;
-    let quorum_id = 0;
+    let matches = command!()
+        .args(&[
+            arg!(-r --"data-root" <HASH> "Set data root"),
+            arg!(-e --epoch <NUM> "Set epoch").value_parser(clap::value_parser!(u64)),
+            arg!(-q --"quorum-id" <ID> "Set quorum id").value_parser(clap::value_parser!(u64)),
+            arg!(-s --server <URL> "Retriever server address"),
+        ])
+        .allow_external_subcommands(true)
+        .get_matches();
 
-    let mut client = RetrieverClient::connect("http://0.0.0.0:34000")
-        .await
-        .unwrap();
+    let data_root = hex::decode(
+        &matches
+            .get_one::<String>("data-root")
+            .expect("data root must provide")[2..],
+    )?;
+    let epoch = *matches.get_one::<u64>("epoch").expect("epoch must provide");
+    let quorum_id = *matches
+        .get_one::<u64>("quorum-id")
+        .expect("quorum id must provide");
+    let server = matches
+        .get_one::<String>("server")
+        .expect("server must provide")
+        .clone();
 
+    let mut client = RetrieverClient::connect(server).await.unwrap();
     let reply = client
         .retrieve_blob(BlobRequest {
             epoch,
             quorum_id,
-            storage_root: data_root.clone(),
+            storage_root: data_root,
         })
         .await
         .unwrap();
--- grpc/src/service.rs
+++ grpc/src/service.rs
@@ -1,12 +1,18 @@
-use std::{sync::Arc, time::Instant};
+use std::{
+    collections::{BTreeMap, HashMap, HashSet},
+    sync::Arc,
+    time::Instant,
+};
 
 use anyhow::bail;
-use contract_provider::ContractProvider;
+use contract_provider::{ContractProvider, SignerDetail, MIN_REQUIRED_SLICE};
+use ethers::types::H160;
 use retriever::{retriever_server::Retriever, BlobReply, BlobRequest};
 use signer_provider::{RetrieveParam, SignerProvider};
 use task_executor::TaskExecutor;
-use tokio::sync::RwLock;
+use tokio::sync::{Mutex, RwLock};
 use tonic::{Code, Request, Response, Status};
+use zg_da_recovery::recover_from_da_slice;
 
 const DEFAULT_MAX_ONGOING_SIGN_REQUEST: u64 = 10;
 
@@ -84,77 +90,180 @@ impl RetrieverService {
     ) -> Result<Response<BlobReply>, Status> {
         let ts = Instant::now();
 
-        let (signers, signer_slices) = self
+        let (signers, signer_first_1024_slices, signer_slices) = self
             .contract_provider
             .get_signers(epoch, quorum_id)
             .await
             .map_err(|e| {
-            Status::new(
-                Code::NotFound,
-                format!("failed get signers from block chain: {:?}", e),
-            )
-        })?;
+                Status::new(
+                    Code::NotFound,
+                    format!("failed get signers from block chain: {:?}", e),
+                )
+            })?;
 
         let mut tasks = vec![];
-        for (address, indices) in signer_slices.into_iter() {
-            let socket = signers
-                .get(&address)
-                .ok_or(Status::new(Code::InvalidArgument, "signer does't exist"))?
-                .socket
-                .clone();
-
-            let signer_provider = self.signer_provider.clone();
-            let data_root = data_root.clone();
-
-            let task = self
-                .executor
-                .spawn_handle(
-                    async move {
-                        let mut response = signer_provider
-                            .get_slices(
-                                socket,
-                                vec![RetrieveParam {
-                                    epoch: epoch,
-                                    quorum_id,
-                                    storage_root: data_root,
-                                    row_indexes: indices.clone(),
-                                }],
-                            )
-                            .await?;
-
-                        match response.pop() {
-                            Some(s) => Ok((indices, s)),
-                            None => {
-                                bail!("slice is empty")
-                            }
-                        }
-                    },
-                    "request slice",
-                )
-                .ok_or(Status::new(Code::Internal, "failed to spawn request slice"))?;
+        let invalid_signers = Arc::new(Mutex::new(HashSet::new()));
+        for (address, indices) in signer_first_1024_slices.into_iter() {
+            let invalid_signers = Arc::clone(&invalid_signers);
+            let task = self.spawn_request_task(
+                &data_root,
+                epoch,
+                quorum_id,
+                address,
+                indices,
+                &signers,
+                invalid_signers,
+            )?;
 
             tasks.push(task);
         }
 
-        let mut slices = vec![];
+        let mut input_slices: BTreeMap<usize, Vec<u8>> = BTreeMap::new();
         for task in tasks {
-            let (indices, s) = task
-                .await
-                .map_err(|e| Status::new(Code::Internal, format!("join error: {:?}", e)))?
-                .ok_or(Status::new(Code::Internal, "failed to run slice"))?
-                .map_err(|e| Status::new(Code::NotFound, format!("fail to get slice: {:?}", e)))?;
-
-            for t in s.into_iter() {
-                slices.push(t);
-            }
+            wait_request_task(task, &mut input_slices).await;
         }
 
-        // todo: construct original data
+        info!("ready slices length {:?}", input_slices.len());
+
+        let mut iter = signer_slices.into_iter();
+        let mut running = true;
+        while input_slices.len() < MIN_REQUIRED_SLICE && running {
+            let mut tasks = vec![];
+            let mut new_requesting_slice_length = 0;
+
+            loop {
+                let (address, indices) = if let Some((address, indices)) = iter.next() {
+                    (address, indices)
+                } else {
+                    error!("no eligible signers available for data request");
+                    running = false;
+                    break;
+                };
+
+                if invalid_signers.lock().await.contains(&address) {
+                    continue;
+                }
+
+                let slice_len = indices.len();
+                let invalid_signers = Arc::clone(&invalid_signers);
+                let task = self.spawn_request_task(
+                    &data_root,
+                    epoch,
+                    quorum_id,
+                    address,
+                    indices,
+                    &signers,
+                    invalid_signers,
+                )?;
+
+                tasks.push(task);
+
+                new_requesting_slice_length += slice_len;
+                if new_requesting_slice_length + input_slices.len() >= MIN_REQUIRED_SLICE {
+                    break;
+                }
+            }
+
+            for task in tasks {
+                wait_request_task(task, &mut input_slices).await;
+            }
+
+            info!("ready slices length {:?}", input_slices.len());
+        }
+
+        info!("start recover {:?} ms", ts.elapsed().as_millis());
         let reply = BlobReply {
-            data: slices[0].clone(),
+            data: recover_from_da_slice(&input_slices).map_err(|e| {
+                Status::new(Code::NotFound, format!("fail to recover slice: {:?}", e))
+            })?,
         };
 
         info!("response in {:?} ms", ts.elapsed().as_millis());
         Ok(Response::new(reply))
+    }
+
+    fn spawn_request_task(
+        &self,
+        data_root: &Vec<u8>,
+        epoch: u64,
+        quorum_id: u64,
+        address: H160,
+        indices: Vec<u32>,
+        signers: &HashMap<H160, SignerDetail>,
+        invalid_signers: Arc<Mutex<HashSet<H160>>>,
+    ) -> Result<
+        task_executor::JoinHandle<Option<Result<(Vec<u32>, Vec<Vec<u8>>), anyhow::Error>>>,
+        Status,
+    > {
+        let mut socket = signers
+            .get(&address)
+            .ok_or(Status::new(Code::InvalidArgument, "signer does't exist"))?
+            .socket
+            .clone();
+
+        if !socket.to_lowercase().starts_with("http://") {
+            socket = format!("http://{}", socket);
+        }
+
+        let signer_provider = self.signer_provider.clone();
+        let data_root = data_root.clone();
+        let task = self
+            .executor
+            .spawn_handle(
+                async move {
+                    let mut response = signer_provider
+                        .get_slices(
+                            socket,
+                            vec![RetrieveParam {
+                                epoch: epoch,
+                                quorum_id,
+                                storage_root: data_root,
+                                row_indexes: indices.clone(),
+                            }],
+                        )
+                        .await?;
+
+                    match response.pop() {
+                        Some(s) => Ok((indices, s)),
+                        None => {
+                            invalid_signers.lock().await.insert(address);
+                            bail!("slice is empty")
+                        }
+                    }
+                },
+                "request slice",
+            )
+            .ok_or(Status::new(Code::Internal, "failed to spawn request slice"))?;
+
+        Ok(task)
+    }
+}
+
+async fn wait_request_task(
+    task: task_executor::JoinHandle<Option<Result<(Vec<u32>, Vec<Vec<u8>>), anyhow::Error>>>,
+    slices: &mut BTreeMap<usize, Vec<u8>>,
+) {
+    match task.await {
+        Ok(r) => match r {
+            Some(v) => match v {
+                Ok((indices, s)) => {
+                    indices
+                        .into_iter()
+                        .zip(s.into_iter())
+                        .for_each(|(key, value)| {
+                            slices.insert(key as usize, value);
+                        });
+                }
+                Err(e) => {
+                    error!("retrieve slice failed, error: {:?}", e);
+                }
+            },
+            None => {
+                error!("slice is None");
+            }
+        },
+        Err(e) => {
+            error!("join error: {:?}", e);
+        }
     }
 }
